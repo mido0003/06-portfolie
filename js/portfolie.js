d91642529a6bf8a705bce8b84dffa617e0014c41
Mie <mido0003@stud.kea.dk>
2025-06-06 12:37:09 +0200

    fjernede noge stylesheet i head

--- js/portfolie.js
+++ js/portfolie.js
@@ -4,13 +4,13 @@ document.querySelector("#pan1").classList.add("pan_animation");
 document.querySelector("#pan1").classList.add("stop");
 
 document.querySelector("#pan1").addEventListener("mouseover", startPanorama);
-document.querySelector("#pan1").addEventListener("mouseout", startPanorama);
+document.querySelector("#pan1").addEventListener("mouseout", stopPanorama);
 
-document.querySelector("#pan2").classList.add("pan_animation");
-document.querySelector("#pan2").classList.add("stop");
+// document.querySelector("#pan2").classList.add("pan_animation");
+// document.querySelector("#pan2").classList.add("stop");
 
-document.querySelector("#pan2").addEventListener("mouseover", startPanorama);
-document.querySelector("#pan2").addEventListener("mouseout", stopPanorama);
+// document.querySelector("#pan2").addEventListener("mouseover", startPanorama);
+// document.querySelector("#pan2").addEventListener("mouseout", stopPanorama);
 
 function startPanorama() {
   this.classList.remove("stop");
